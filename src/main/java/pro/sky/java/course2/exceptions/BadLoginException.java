295e6732559d839625acbb1897b8b57058cd2bbd
Solpostgit <begemot_off@mail.ru>
2023-10-26 21:00:20 +0300

    Изменил имя класса WrongLoginException

--- src/main/java/pro/sky/java/course2/exceptions/BadLoginException.java
+++
@@ -1,4 +0,0 @@
-package pro.sky.java.course2.exceptions;
-
-public class BadLoginException extends RuntimeException {
-}
---
+++ src/main/java/pro/sky/java/course2/exceptions/WrongLoginException.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course2.exceptions;
+
+public class WrongLoginException extends RuntimeException {
+}
